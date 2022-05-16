6ae6a76d1c933e88610ab5b3675c0ce595409e93
Gustavo Analise Ferreira <63479834+gustavoanalise@users.noreply.github.com>
2022-05-16 14:29:00 -0300

    Links para usuário autenticado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
 import { NavLink } from "react-router-dom";
 import styles from './Navbar.module.css';
+import { useAuthentication } from "../hooks/useAuthentication";
+import { useAuthValue } from "../context/AuthContext";
 
 const Navbar = () => {
+    const { user } = useAuthValue();
+
+
     return (
         <nav className={styles.navbar}>
             <NavLink to='/' className={styles.brand}>
@@ -9,16 +14,30 @@ const Navbar = () => {
             </NavLink>
             <ul className={styles.links_list}>
                 <li>
-                    <NavLink to='/' className={({isActive}) => (isActive ? styles.active : "")}>Home</NavLink>
+                    <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : "")}>Home</NavLink>
                 </li>
+                {!user && (
+                    <>
+                        <li>
+                            <NavLink to='/login' className={({ isActive }) => (isActive ? styles.active : "")}>Login</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/register' className={({ isActive }) => (isActive ? styles.active : "")}>Cadastre-se</NavLink>
+                        </li>
+                    </>
+                )}
+                {user && (
+                    <>
+                        <li>
+                            <NavLink to='/posts/create' className={({ isActive }) => (isActive ? styles.active : "")}>Publicar</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/dashboard' className={({ isActive }) => (isActive ? styles.active : "")}>Dashboard</NavLink>
+                        </li>
+                    </>
+                )}
                 <li>
-                    <NavLink to='/login' className={({isActive}) => (isActive ? styles.active : "")}>Login</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/register' className={({isActive}) => (isActive ? styles.active : "")}>Cadastre-se</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/about' className={({isActive}) => (isActive ? styles.active : "")}>Sobre</NavLink>
+                    <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : "")}>Sobre</NavLink>
                 </li>
             </ul>
         </nav>
---
+++ src/pages/CreatePost/CreatePost.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './CreatePost.module.css'
+
+const CreatePost = () => {
+    return (
+        <div>
+            <h1>Crie seu Post</h1>
+        </div>
+    )
+}
+
+export default CreatePost
