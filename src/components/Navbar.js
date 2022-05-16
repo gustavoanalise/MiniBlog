12bc9db7c263ccbad0c9744a3972c147f6bfa97e
Gustavo Analise Ferreira <63479834+gustavoanalise@users.noreply.github.com>
2022-05-16 15:23:41 -0300

    Bloqueando páginas que precisam de autenticação e página about concluída

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,6 +16,7 @@ const Navbar = () => {
                 <li>
                     <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : "")}>Home</NavLink>
                 </li>
+
                 {!user && (
                     <>
                         <li>
@@ -26,6 +27,7 @@ const Navbar = () => {
                         </li>
                     </>
                 )}
+
                 {user && (
                     <>
                         <li>
@@ -36,6 +38,7 @@ const Navbar = () => {
                         </li>
                     </>
                 )}
+                
                 <li>
                     <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : "")}>Sobre</NavLink>
                 </li>
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,24 @@
 // CSS
 import styles from './About.module.css';
-
+import { FaGithubSquare, FaLinkedin, FaReact } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className={styles.about}>
+            <h2>Sobre o Mini Blog usando o <span>ReactJs</span></h2>
+
+            <h1><FaReact /></h1>
+
+            <p>A ideia do Projeto foi de desenvolver um blog utilizando o ReactJS no FrontEnd, usando assim suas funcionalidades visando uma navegação mais amigável ao cliente.
+            No BackEnd utilizamos o Firebase da Google para ajudar no processo de criação do usuário, autenticação, tratamento de erros, criação de posts, entre outras funções.</p>
+
+            <p>Veja mais:</p>
+            <p><a href="https://github.com/gustavoanalise/" target="_blank" rel="noopener noreferrer"><FaGithubSquare /> GitHub</a></p>
+            <a href="https://www.linkedin.com/in/gustavo-analise-a85738133/" target="_blank" rel="noopener noreferrer"><FaLinkedin /> LinkedIn</a>
+
+            <Link to='/posts/create' className='btn'>
+                Criar Post
+            </Link>
         </div>
     )
 }
